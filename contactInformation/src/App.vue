f13f3727f102a19defbfaadc58ae22a4da1a00cd
kuankuan2007 <2163826131@qq.com>
2025-04-21 23:22:35 +0800

    20250121

--- contactInformation/src/App.vue
+++ contactInformation/src/App.vue
@@ -8,8 +8,28 @@
           <h2>QQ</h2>
         </div>
         <div>
-          <links tips="添加好友" copy-message="3169748076" href="https://qm.qq.com/q/uyUcVvYqWs" content="福瑞 : 3169748076" />
-          <links tips="添加好友" copy-message="2163826131" href="https://qm.qq.com/q/kGOteCwCFq" content="其他 : 2163826131" />
+          <links
+            tips="添加好友"
+            copy-message="3169748076"
+            :qrcode="qqQr1"
+            :href="
+              mobile
+                ? 'mqqopensdkapi://bizAgent/qm/qr?url=https%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Fk%3Do6LlD46UQQTkCovv8LlDPnNmivremQoj%26jump_from%3D%26auth%3D%26app_name%3D%26authSig%3D%26source_id%3D3_40001'
+                : 'tencent://ntqq-open?subCmd=profile&action=openMiniBuddyProfile&actionParams=%7B%22uin%22%3A%223169748076%22%2C%22sourceType%22%3A%22QrCodeShareBuddyLink%22%7D'
+            "
+            content="福瑞 : 3169748076"
+          />
+          <links
+            tips="添加好友"
+            copy-message="2163826131"
+            :qrcode="qqQr2"
+            :href="
+              mobile
+                ? 'mqqopensdkapi://bizAgent/qm/qr?url=https%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Fk%3DhYDeG_BAcHg23FikRpLckbGvcLsyd9Em%26jump_from%3D%26auth%3D%26app_name%3D%26authSig%3D%26source_id%3D3_40001'
+                : 'tencent://ntqq-open?subCmd=profile&action=openMiniBuddyProfile&actionParams=%7B%22uin%22%3A%222163826131%22%2C%22sourceType%22%3A%22QrCodeShareBuddyLink%22%7D'
+            "
+            content="其他 : 2163826131"
+          />
         </div>
       </div>
       <div>
@@ -17,7 +37,11 @@
           <h2>QQ频道</h2>
         </div>
         <div>
-          <links href="https://pd.qq.com/s/7iqf08bgx" copy-message="kuankuan3World" content="宽宽的小天地" />
+          <links
+            href="https://pd.qq.com/s/7iqf08bgx"
+            copy-message="kuankuan3World"
+            content="宽宽的小天地"
+          />
         </div>
       </div>
       <div>
@@ -25,10 +49,18 @@
           <h2>微信</h2>
         </div>
         <div>
-          <links tips="添加好友" copy-message="furrykuankuan" href="https://u.wechat.com/MIuqPCR_YOvP88wrhJBtuXI"
-            content="福瑞 : furrykuankuan" />
-          <links tips="添加好友" copy-message="gouhaoming2007" href="https://u.wechat.com/ML4NzEKd45FRDTKi1Uj4ZaA"
-            content="其他 : gouhaoming2007" />
+          <links
+            tips="添加好友"
+            copy-message="furrykuankuan"
+            href="https://u.wechat.com/MIuqPCR_YOvP88wrhJBtuXI"
+            content="福瑞 : furrykuankuan"
+          />
+          <links
+            tips="添加好友"
+            copy-message="gouhaoming2007"
+            href="https://u.wechat.com/ML4NzEKd45FRDTKi1Uj4ZaA"
+            content="其他 : gouhaoming2007"
+          />
         </div>
       </div>
 
@@ -37,7 +69,11 @@
           <h2>bilibili</h2>
         </div>
         <div>
-          <links copy-message="宽宽2007" href="https://space.bilibili.com/662698080" content="宽宽2007" />
+          <links
+            copy-message="宽宽2007"
+            href="https://space.bilibili.com/662698080"
+            content="宽宽2007"
+          />
         </div>
       </div>
       <div>
@@ -45,9 +81,13 @@
           <h2>抖音</h2>
         </div>
         <div>
-          <links copy-message="kuankuan2007" :qrcode="douyinURL" tips="抖音扫一扫"
+          <links
+            copy-message="kuankuan2007"
+            :qrcode="douyinURL"
+            tips="抖音扫一扫"
             href="https://www.douyin.com/user/MS4wLjABAAAA5m3gNGWqC6LuoDid1MIqfReLSm9QhBgZDIcuGvf1v8M"
-            content="kuankuan2007" />
+            content="kuankuan2007"
+          />
         </div>
       </div>
       <div>
@@ -55,9 +95,12 @@
           <h2>X</h2>
         </div>
         <div>
-          <links copy-message="kuankuan2007" tips="用浏览器打开"
+          <links
+            copy-message="kuankuan2007"
+            tips="用浏览器打开"
             href="https://x.com/kuankuan2007"
-            content="kuankuan2007" />
+            content="kuankuan2007"
+          />
         </div>
       </div>
       <div>
@@ -65,9 +108,12 @@
           <h2>YouTube</h2>
         </div>
         <div>
-          <links copy-message="kuankuan2007" tips="用浏览器打开"
+          <links
+            copy-message="kuankuan2007"
+            tips="用浏览器打开"
             href="https://www.youtube.com/@kuankuan2007"
-            content="kuankuan2007" />
+            content="kuankuan2007"
+          />
         </div>
       </div>
       <div>
@@ -75,9 +121,12 @@
           <h2>Github</h2>
         </div>
         <div>
-          <links copy-message="kuankuan2007" tips="用浏览器打开"
+          <links
+            copy-message="kuankuan2007"
+            tips="用浏览器打开"
             href="https://www.github.com/kuankuan2007"
-            content="kuankuan2007" />
+            content="kuankuan2007"
+          />
         </div>
       </div>
       <div>
@@ -85,9 +134,12 @@
           <h2>Gitee</h2>
         </div>
         <div>
-          <links copy-message="kuankuan2007" tips="用浏览器打开"
+          <links
+            copy-message="kuankuan2007"
+            tips="用浏览器打开"
             href="https://www.gitee.com/kuankuan2007"
-            content="kuankuan2007" />
+            content="kuankuan2007"
+          />
         </div>
       </div>
       <div>
@@ -95,9 +147,11 @@
           <h2>Pypi</h2>
         </div>
         <div>
-          <links copy-message="kuankuan" 
+          <links
+            copy-message="kuankuan"
             href="https://pypi.org/user/kuankuan/"
-            content="kuankuan" />
+            content="kuankuan"
+          />
         </div>
       </div>
       <div>
@@ -105,9 +159,11 @@
           <h2>NPM</h2>
         </div>
         <div>
-          <links copy-message="kuankuan" 
+          <links
+            copy-message="kuankuan"
             href="https://www.npmjs.com/~kuankuan"
-            content="kuankuan" />
+            content="kuankuan"
+          />
         </div>
       </div>
       <div>
@@ -115,26 +171,47 @@
           <h2>邮箱</h2>
         </div>
         <div>
-          <links copy-message="3169748076@qq.com" tips="发送邮件" href="mailto:3169748076@qq.com" content="福瑞/收稿 : 3169748076@qq.com" />
-          <links copy-message="2163826131@qq.com" tips="发送邮件" href="mailto:2163826131@qq.com" content="其他 : 2163826131@qq.com" />
+          <links
+            copy-message="3169748076@qq.com"
+            tips="发送邮件"
+            href="mailto:3169748076@qq.com"
+            content="福瑞/收稿 : 3169748076@qq.com"
+          />
+          <links
+            copy-message="2163826131@qq.com"
+            tips="发送邮件"
+            href="mailto:2163826131@qq.com"
+            content="其他 : 2163826131@qq.com"
+          />
         </div>
       </div>
     </ul>
   </div>
 </template>
 <script setup>
-import { onMounted } from "vue";
-import myheader from "../../src/common/components/header.vue"
-import showBG from "./back"
-import links from "./components/links.vue";
-import douyinURL from "./assets/douyin.png"
+import { onMounted, ref } from 'vue';
+import myheader from '../../src/common/components/header.vue';
+import showBG from './back';
+import links from './components/links.vue';
+import douyinURL from './assets/douyin.png';
+import { getQRCode, isMobie } from '../../src/common/script/normal';
+
+const mobile = isMobie();
+const qqQr1 = ref('1');
+const qqQr2 = ref('2');
+getQRCode('https://qm.qq.com/q/GFHAYacPAY').then((res) => {
+  qqQr1.value = res;
+});
+getQRCode('https://qm.qq.com/q/WLNfjovveQ').then((res) => {
+  qqQr2.value = res;
+});
 
 onMounted(() => {
-  showBG()
-})
+  showBG();
+});
 </script>
 <style scoped lang="scss">
-.full{
+.full {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -144,15 +221,12 @@ onMounted(() => {
   display: grid;
   row-gap: 10px;
 
-  &>div {
+  & > div {
     &::after {
-      content: "";
+      content: '';
       width: 100%;
       height: 1px;
-      background: linear-gradient(90deg,
-          transparent,
-          var(--font-color),
-          transparent);
+      background: linear-gradient(90deg, transparent, var(--font-color), transparent);
       position: absolute;
       bottom: -5px;
       transform: translate(0, 50%);
@@ -162,8 +236,8 @@ onMounted(() => {
     display: flex;
     align-items: center;
 
-    &>div.title {
-      &>h2 {
+    & > div.title {
+      & > h2 {
         margin: 0;
       }
     }
@@ -173,15 +247,15 @@ onMounted(() => {
       text-decoration: none;
     }
 
-    &>div {
+    & > div {
       margin-left: 20px;
 
-      &>* {
+      & > * {
         white-space: nowrap;
         word-break: keep-all;
       }
 
-      &>p {
+      & > p {
         margin: 0;
         color: black;
       }
--- contactInformation/src/components/links.vue
+++ contactInformation/src/components/links.vue
@@ -1,48 +1,54 @@
 <template>
-    <div :class="{
-        mobie: mobile
-    }">
-        <div @click="onclick" :href="props.href">
-            <div>
-                <a :href="props.href">{{ props.content }}</a>
-                <linkLikeButton @click.stop="copy" class="icon"><k-icons icon="copy" /></linkLikeButton>
-            <div class="qrcode">
-                <p>{{ props.tips }}</p>
-                <img :src="qr">
-            </div>
-            </div>
+  <div
+    :class="{
+      mobie: mobile,
+    }"
+  >
+    <div @click="onclick" :href="props.href">
+      <div>
+        <a :href="props.href">{{ props.content }}</a>
+        <linkLikeButton @click.stop="copy" class="icon"><k-icons icon="copy" /></linkLikeButton>
+        <div class="qrcode">
+          <p>{{ props.tips }}</p>
+          <img :src="qr" />
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
-import { ref } from "vue";
-import { copyText, jump } from "../../../src/common/script/normal";
-import { askChoice, showImages, showMessage } from "../../../src/common/script/infomations"
-import { getQRCode, isMobie } from "../../../src/common/script/normal"
-import linkLikeButton from "../../../src/common/components/input/linkLikeButton.vue";
-import KIcons from "../../../src/common/components/KIcons.vue";
+import { ref, watchEffect } from 'vue';
+import { copyText, jump } from '../../../src/common/script/normal';
+import { askChoice, showImages, showMessage } from '../../../src/common/script/infomations';
+import { getQRCode, isMobie } from '../../../src/common/script/normal';
+import linkLikeButton from '../../../src/common/components/input/linkLikeButton.vue';
+import KIcons from '../../../src/common/components/KIcons.vue';
 let props = defineProps({
-    href: {
-        type: String,
-        required: true
-    }, content: {
-        typr: String,
-        required: true
-    }, tips: {
-        type: String,
-        required: false,
-        default: ""
-    }, qrcode: {
-        type: String,
-        required: false
-    }, copyMessage: {
-        type: String,
-        required: false,
-    }
-})
-async function copy(){
-    await copyText(props.copyMessage)
-    showMessage("复制成功")
+  href: {
+    type: String,
+    required: true,
+  },
+  content: {
+    typr: String,
+    required: true,
+  },
+  tips: {
+    type: String,
+    required: false,
+    default: '',
+  },
+  qrcode: {
+    type: String,
+    required: false,
+  },
+  copyMessage: {
+    type: String,
+    required: false,
+  },
+});
+async function copy() {
+  await copyText(props.copyMessage);
+  showMessage('复制成功');
 }
 /**
  * Handles the onclick event.
@@ -51,81 +57,82 @@ async function copy(){
  * @return {void} This function does not return a value.
  */
 function onclick(e) {
-    if (mobile) {
-        e.preventDefault()
-        askChoice("请选择链接打开方式", "询问", true, {
-            "跳转": "jump",
-            "二维码展示": "qrcode"
-        }).then(result => {
-            if (result === "jump") {
-                jump(props.href, true)
-            }
-            else {
-                showImages([qr.value])
-            }
-        }, reason => { })
-    }
+  if (mobile) {
+    e.preventDefault();
+    askChoice('请选择链接打开方式', '询问', true, {
+      跳转: 'jump',
+      二维码展示: 'qrcode',
+    }).then(
+      (result) => {
+        if (result === 'jump') {
+          jump(props.href, true);
+        } else {
+          showImages([qr.value]);
+        }
+      },
+      (reason) => {}
+    );
+  }
 }
-let mobile = isMobie()
-let qr = ref(null)
+let mobile = isMobie();
+let qr = ref(null);
 
 if (props.qrcode) {
-    qr.value = props.qrcode
+  watchEffect(() => {
+    qr.value = props.qrcode;
+  });
 } else {
-    getQRCode(props.href).then((dataURL) => {
-        qr.value = dataURL
-    })
+  getQRCode(props.href).then((dataURL) => {
+    qr.value = dataURL;
+  });
 }
-
 </script>
 <style scoped lang="scss">
 a {
-    color: var(--font-color);
-    cursor: default;
+  color: var(--font-color);
+  cursor: default;
 
-
-    text-decoration: none;
-    cursor: pointer;
-    color: var(--theme-strong1);
-
+  text-decoration: none;
+  cursor: pointer;
+  color: var(--theme-strong1);
 }
 
 div.qrcode {
-    position: absolute;
-    top: 100%;
-    left: 50%;
-    opacity: 0;
-    pointer-events: none;
-    overflow: hidden;
-    transform: translate(-50%, -50%);
-    transition: 0.3s;
-    background-color: #fff;
-    z-index: 1;
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  opacity: 0;
+  pointer-events: none;
+  overflow: hidden;
+  transform: translate(-50%, -50%);
+  transition: 0.3s;
+  background-color: #fff;
+  z-index: 1;
+  border-radius: calc(10px * var(--theme-border-radius));
+
+  .mobie & {
+    display: none;
+  }
+
+  div:hover > & {
+    transform: translate(-50%, 0%);
+    opacity: 1;
+  }
+
+  & > img {
     border-radius: calc(10px * var(--theme-border-radius));
+  }
 
-    .mobie & {
-        display: none;
-    }
-
-    div:hover>& {
-        transform: translate(-50%, 0%);
-        opacity: 1;
-    }
-
-    &>img {
-        border-radius: calc(10px * var(--theme-border-radius));
-    }
-
-    &>p {
-        margin: 0;
-        margin-top: 5px;
-        margin-bottom: -5px;
-        text-align: center;
-        color: black;
-    }
+  & > p {
+    margin: 0;
+    margin-top: 5px;
+    margin-bottom: -5px;
+    text-align: center;
+    color: black;
+  }
 }
 
 p {
-    position: relative;
+  position: relative;
 }
 </style>
